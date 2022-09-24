08a4c0c7511f04188baff0ccb2b4c80d8f63d256
Paulo <trigopaulo@gmail.com>
2022-09-24 16:26:44 -0300

    project finished

--- Snake_Game/Assets/Scripts/Snake.cs
+++ Snake_Game/Assets/Scripts/Snake.cs
@@ -6,15 +6,16 @@ public class Snake : MonoBehaviour
 {
     //declaring a variable
     private Vector2 direction = Vector2.right;
-    private  List<Transform> segments;
+    private  List<Transform> segments = new List<Transform>();
     public Transform segmentPrefab;
+
+    public int initialSize = 4;
 
     //Start the Snake's segments
     
     private void Start() {
 
-        segments = new List<Transform>();
-        segments.Add(this.transform);
+        ResetState();
     }
 
     private void Update(){
@@ -34,31 +35,55 @@ public class Snake : MonoBehaviour
             direction = Vector2.right;
         }
     }
+
     private void FixedUpdate(){
+
+        for (int i = segments.Count - 1; i > 0; i--){
+            segments[i].position = segments[i - 1].position;
+        }
         
         this.transform.position = new Vector3(
             Mathf.Round(this.transform.position.x )+  direction.x,
             Mathf.Round(this.transform.position.y )+  direction.y,
             0.0f
         );
-
     }
 
 //Growing the Snake
     private void Grow(){
 
         Transform segment = Instantiate(this.segmentPrefab);
-        segment.position = segments[segments.Count -1].position;
+        segment.position = segments[segments.Count - 1].position;
 
         segments.Add(segment);
     }
 
-    private void OnTriggerEnter2D(Collider2D other) {
+//Reseting the State
+    private void ResetState(){
+
+        for (int i = 1; i < segments.Count; i++){
+            Destroy(segments[i].gameObject);
+        }
+
+        segments.Clear();
+        segments.Add(this.transform);
+
+        for (int i = 1; i < this.initialSize; i++){
+            segments.Add(Instantiate(this.segmentPrefab));        
+        }
+
+        this.transform.position = Vector3.zero;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other){
         
         if (other.tag == "Food"){
             Grow();
         }
-    }
 
+        else if (other.tag == "Obstacle"){
+            ResetState();
+        }
+    }
 }
 
