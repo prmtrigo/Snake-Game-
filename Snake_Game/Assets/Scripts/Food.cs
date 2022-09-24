a12ec36fa2a0b1a276b55d6de714a86339a812d0
Paulo <trigopaulo@gmail.com>
2022-09-24 14:13:07 -0300

    food trigger

--- Snake_Game/Assets/Scripts/Food.cs
+++ Snake_Game/Assets/Scripts/Food.cs
@@ -4,5 +4,35 @@ using UnityEngine;
 
 public class Food : MonoBehaviour
 {
-    
+    //due to trigger and boxgrid, food can't spawn on the walls
+
+    public BoxCollider2D gridArea;
+
+    private void Start(){
+
+        RandomizePosition();
+    }
+    //now, we have to randomize the food position!!
+
+    private void RandomizePosition(){
+
+        Bounds bounds =  this.gridArea.bounds;
+
+        float x = Random.Range(bounds.min.x, bounds.max.x);
+        float y = Random.Range(bounds.min.y, bounds.max.y);
+
+        this.transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), 0.0f);
+
+    }
+
+    //When Trigger is Activated, Food goes to another Random Position!
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        
+        if (other.tag == "Player"){
+        RandomizePosition();
+        }
+
+    }
 }
+
